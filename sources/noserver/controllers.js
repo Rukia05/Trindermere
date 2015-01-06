d8f5baa6da8c333e1a35c3ac1867960ec9d9b566
Cyprien Cattelain <cyprien.cattelain@insa-lyon.fr>
2015-01-06 15:57:06 +0100

    cf commit précédent

---
+++ sources/noserver/controllers.js
@@ -0,0 +1,22 @@
+var carteApp = angular.module('carteApp', []);
+
+carteApp.controller('MainCtrl',['$scope',function ($scope) {
+	$scope.title = "PDC8 - Carte"
+    $scope.latitude = "";
+    $scope.longitude = "";
+    $scope.idarbre = "";
+    
+    $scope.addBase = function(latitude,longitude,identifiant) {
+    	$.ajax({
+            url: 'https://rencontres-arbres.herokuapp.com/api/trees/AR17026/',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            username: 'arborphile',
+            password: 'ilovetrees',
+            type: 'PUT',
+            data: '{"name":"fjdlk fhg"}'
+        });
+        alert("ajouter dans la base : "+latitude+','+longitude+','+identifiant);
+
+    }
+}]);
