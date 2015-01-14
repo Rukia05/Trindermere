f09eb3d359e1221483158bd771d3f8b267779ad0
Julia Reboul <julia.reboul@gmail.com>
2015-01-14 14:12:00 +0100

    Modification avec index.html de JM, intégration des services

--- sources/static/controllers.js
+++ sources/static/controllers.js
@@ -8,7 +8,7 @@ carteApp.config(function($httpProvider) {
     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 });
 
-carteApp.factory('API', function($http) {
+/*carteApp.factory('API', function($http) {
     return {
         majPosition: function(url, longitude, latitude) {
             return $http({
@@ -22,14 +22,57 @@ carteApp.factory('API', function($http) {
             });
         }
     };
-});
+});*/
+
+carteApp.controller('TreeListCtrl', function($scope, API) {
+    $scope.trees = [];
+    API.getTrees().then(function(trees) {
+        /*$scope.messages.push({
+          type: 'success',
+          message: 'Tree list fully loaded'
+        });*/
+        console.log("tree list fully loaded");
+        $scope.trees = trees;
+    }, 
+    function() { // error handling
+        $scope.messages.push({
+          type: 'danger',
+          message: 'Tree list loading failed'
+        });
+      }, 
+    function(trees) { // update
+        //$scope.messages.push({
+        //  type: 'info',
+        //  message: 'Tree list updated'
+        //});
+    $scope.trees.push.apply($scope.trees, trees);
+      });
+    });
+/*{
+              longitude: longInput.value,
+              latitude: latInput.value,
+            }*/
+
+carteApp.controller('TreePatchCtrl', function($scope, $http, API) {
+    console.log(API);
+    $scope.patch = function() {
+            var id = $scope.idarbre;
+            var data = {longitude: longInput.value, latitude: latInput.value};
+            console.log("data " + data);
+            API.patchTree(id, data).then(function() {
+              $scope.messages.push({
+                type: 'success',
+                message: 'Tree ' + id + ' patched'
+              });
+            });
+          };
+        });
 
 carteApp.controller('MainCtrl', function ($scope, API) {
-	$scope.title = "PDC8 - Carte"
+	console.log("ici c'est le main");
+    $scope.title = "PDC8 - Carte"
     $scope.latitude = "";
     $scope.longitude = "";
     $scope.idarbre = "";
     
-    API.majPosition('http://rencontres-arbres.herokuapp.com/api/trees/AR17026/', 12.3, 15.4);
-    //console.log("ajouter dans la base : "+latitude+','+longitude+','+identifiant);
 });
