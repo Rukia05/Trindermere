cc4ae0ce2e0bdb776439ddeb7ea6ea8fb957a04e
Julia Reboul <julia.reboul@gmail.com>
2015-01-17 13:27:56 +0100

    Ajout de la fonction pour détecter les doublons, même si elle sert à rien pour l'instant, elle est fonctionnelle

--- static/controllers.js
+++ static/controllers.js
@@ -1,12 +1,27 @@
 carteApp.controller('TreeListCtrl', function($scope, API, $filter) {
     $scope.trees = [];
     API.getTrees().then(function(trees) {
+        var doub = false;
         console.log("tree list fully loaded");
         $scope.trees = trees;
+        //console.log("url "+ trees[200].url);
+        for(i=0; i<trees.length;i++){
+            for(j=0;j<trees.length;j++){
+                if(trees[i].url==trees[j].url && (trees[i].latitude!==trees[j].latitude || trees[i].longitude!==trees[j].longitude)){
+                    console.log("Doublon!! url i = "+ trees[i].url + "url j = " +trees[j].url + "lat i " + trees[i].latitude + "lat j " + trees[j].latitude + "long i " + trees[i].longitude + "long j" + trees[j].longitude);
+                    doub = true;
+                }
+            }
+        }
+        if(doub==false){
+            console.log("Pas de doublon détecté.");
+        }
+
+        //console.log("taille " + trees.length);
     }, function() { // error handling
     }, function(trees) { // update
         $scope.trees.push.apply($scope.trees, trees);
-      });
+    });
     });
 
 carteApp.controller('TreePatchCtrl', function($scope, $http, API) {
